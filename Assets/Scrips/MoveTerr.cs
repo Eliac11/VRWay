ebab69d8d30e8284bdf1d7ad68d8ca864839f40e
Eliac11 <58089493+Eliac11@users.noreply.github.com>
2021-09-19 19:31:50 +0300

    о ура что-то рабочее

---
+++ Assets/Scrips/MoveTerr.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveTerr : MonoBehaviour
+{
+    //public GameObject Terr;
+    public float speed;
+
+    public Transform startPos;
+    public Transform EndPos;
+
+    public Transform[] terrs;
+
+    private float StartZ;
+    private float EndZ;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        //terrs = Terr.GetComponentsInChildren<Transform>();
+
+        StartZ = startPos.position.z;
+        EndZ = EndPos.position.z;
+
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        foreach(Transform i in terrs)
+        {
+            i.position += new Vector3(0, 0, speed);
+
+            if (i.position.z >= EndZ)
+            {
+                i.position = new Vector3(i.position.x, i.position.y, StartZ);
+            }
+        }
+    }
+}
