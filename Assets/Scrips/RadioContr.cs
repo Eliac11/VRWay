8d3bdd373cc1bdde80dfe791d529d668b15b1e37
Eliac11 <58089493+Eliac11@users.noreply.github.com>
2021-09-21 20:19:19 +0300

    норм получилось

---
+++ Assets/Scrips/RadioContr.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RadioContr : MonoBehaviour
+{
+    public AudioClip[] allmusic;
+
+    private AudioSource audioS;
+
+    public float leghtnowclip;
+    // Start is called before the first frame update
+    void Start()
+    {
+        audioS = GetComponent<AudioSource>();
+        SetRandomclip();
+        Startplay();
+        
+    }
+
+    public void SetRandomclip()
+    {
+        AudioClip c = allmusic[Random.Range(0, allmusic.Length)];
+        if (c != audioS.clip)
+        {
+                leghtnowclip = c.length;
+                audioS.clip = c;
+        }
+    }
+    public void Startplay()
+    {
+        audioS.Play();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        leghtnowclip -= Time.deltaTime;
+        if (leghtnowclip < 0)
+        {
+            SetRandomclip();
+            audioS.Play();
+        }
+    }
+}
